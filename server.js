7614d165b6efb2d308574c23a5a103345416b4bc
subash123r <rssubash96@gmail.com>
2026-09-24 12:12:34 +0530

    Update backend

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Expense Tracker Backend is running 🚀"
+  });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/salary", salaryRoutes);
